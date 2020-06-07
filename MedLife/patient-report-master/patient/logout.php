d417f675637e612889aa8fb4b525c4e0ee2dd787
singh-prachi <singh_prachi@apple.com>
2020-06-07 14:28:05 +0530

    removed redundant modules

--- MedLife/patient-report-master/patient/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-setcookie(session_name(), '', 100);
-$_SESSION = array();
-session_destroy();
-header('Location: /index.php');
-exit();
-?>
--- MedLife/patient-report-master/patient/report_img.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-echo '<img src='.$_GET['url'].'>';
-?>
